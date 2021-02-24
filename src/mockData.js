4e116a5f61c25f9c2f3f9430963de59a1a90ca58
mattdean <matthewallandean@gmail.com>
2021-02-23 17:41:28 -0700

    Decide on crop template

---
+++ src/mockData.js
@@ -0,0 +1,14 @@
+const crops = [
+  {
+    id: Number, 
+    name: String,
+    description: String,
+    sunReqs: String,
+    sowingMethod: String,
+    spread: Number,
+    rowSpacing: Number,
+    height: Number,
+    daysToMaturity: Number,
+    photoLinks: [String, String]
+  },
+]
