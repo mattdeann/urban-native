de6bec9dc7f333c81f5b9ce8ea4b27af77d510ad
mattdean <matthewallandean@gmail.com>
2021-02-23 17:46:22 -0700

    Finish mock data templates

--- src/mockData.js
+++ src/mockData.js
@@ -3,6 +3,7 @@ const crops = [
     id: Number, 
     name: String,
     description: String,
+    // Description is a plant description followed by care instructions
     sunReqs: String,
     sowingMethod: String,
     spread: Number,
@@ -11,4 +12,13 @@ const crops = [
     daysToMaturity: Number,
     photoLinks: [String, String]
   },
+]
+
+const users = [
+  {
+    id: Number,
+    name: String,
+    myGarden: [cropID, cropID]
+    // to build garden, iterate through cropIDs, find each one, and use that built array
+  }
 ]
