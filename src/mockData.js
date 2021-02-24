2f937a076c91e202cf2338314183bf3b48a63301
Brian Forbes <brianandrewf81@gmail.com>
2021-02-23 18:44:48 -0700

    Add kale details to mock data

--- src/mockData.js
+++ src/mockData.js
@@ -39,32 +39,39 @@ const crops = [
     // measure in centimeters
     height: 20,
     // measure in centimeters
-    daysToMaturity: `It mainly depends on variety, weather conditions, nutrients etc.,
+    daysToMaturity:
+      `It mainly depends on variety, weather conditions, nutrients etc.,
       Generally cucumber plants take approximately 55 to 70 days from germination to harvesting stage.
       1. The germination stage alone takes 7 to 10 days.
       2. After germination the first male flower would be seen within 35 to 55 days roughly, which will be later followed by developing a female flower in one or two weeks (i.e., 42 to 62 days).
       3. The fertilized female flower will take 10 to 12 days to produce fruits.`,
     photoLinks: [`https://www.thespruce.com/thmb/otEJQ4c7WgHNKUVJNFsTr5Opo4A=/960x0/filters:no
       _upscale():max_bytes(150000):strip_icc():format(webp)/companion-plants-for-cucumbers-2540044-16-d691b5c0f19f44a8b1303e805be9c99d.jpg`]
-  },{
+  },
+  {
     id: 3,
     key: Date.now(),
     name: `Kale`,
-    description: `Kale is a cultivar of the species Brassica oleracea. It has green or purple leaves that
+    description:
+     `Kale is a cultivar of the species Brassica oleracea. It has green or purple leaves that
       branch off from one to multiple upright stems and do not form a head like cabbage. The plant is usually
       grown as an annual, but if grown as a perennial, it will form seeds in the second year. Current popular
       varieties include Curly kale, Italian kale, and Red Russian kale (green leaves with pale purple stems).
       It can be grown as baby salad greens or for bunching adult leaves. Leaves are sweeter after a frost and
       delicious eaten raw, added to salads, sautéed, or added to stews and casseroles.`,
     // Description is a plant description followed by care instructions
-    sunReqs: String,
-    sowingMethod: String,
-    spread: Number,
-    rowSpacing: Number,
-    height: Number,
-    daysToMaturity: Number,
-    photoLinks: [String, String]
-  },{
+    sunReqs: `Full Sun`,
+    sowingMethod: `Direct seed. If planting indoors, harden off before transplanting seedlings outside.`,
+    spread: 75,
+    rowSpacing: 45,
+    height: 75,
+    daysToMaturity:
+     `Kale has a relatively fast growth rate and can grow from seed to harvest in about two months.
+      It is a biennial plant that typically is grown as an annual. It is best planted in spring
+      in cooler climates, and in late summer in warmer climates.`,
+    photoLinks: [`thumbnail_url": "https://s3.amazonaws.com/openfarm-project/production/media/pictures/attachments/55b6b7ce6465380003910200.jpg?1438037945`]
+  },
+  {
     id: 4,
     key: Date.now(),
     name: String,
@@ -77,7 +84,8 @@ const crops = [
     height: Number,
     daysToMaturity: Number,
     photoLinks: [String, String]
-  },{
+  },
+  {
     id: 5,
     key: Date.now(),
     name: String,
@@ -90,7 +98,8 @@ const crops = [
     height: Number,
     daysToMaturity: Number,
     photoLinks: [String, String]
-  },{
+  },
+  {
     id: 6,
     key: Date.now(),
     name: String,
@@ -103,7 +112,8 @@ const crops = [
     height: Number,
     daysToMaturity: Number,
     photoLinks: [String, String]
-  },{
+  },
+  {
     id: 7,
     key: Date.now(),
     name: String,
@@ -116,7 +126,8 @@ const crops = [
     height: Number,
     daysToMaturity: Number,
     photoLinks: [String, String]
-  },{
+  },
+  {
     id: 8,
     key: Date.now(),
     name: String,
@@ -129,7 +140,8 @@ const crops = [
     height: Number,
     daysToMaturity: Number,
     photoLinks: [String, String]
-  },{
+  },
+  {
     id: 9,
     key: Date.now(),
     name: String,
@@ -142,7 +154,8 @@ const crops = [
     height: Number,
     daysToMaturity: Number,
     photoLinks: [String, String]
-  },{
+  },
+  {
     id: 10,
     key: Date.now(),
     name: String,
