9745c5ba3d8a18b2e21ac8a70b5207632468c608
Cameron Aragon <caragon4695@gmail.com>
2021-02-25 18:19:28 -0700

    Fix API fetch path

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -1,11 +1,11 @@
 const getCrops = () => {
-  return fetch("http://localhost:3000/api/v1/crops")
+  return fetch("http://localhost:3001/api/v1/crops")
   .then(response => response.json())
   .then(data => data.crops)
 }
 
 const getUser = id => {
-  return fetch(`http://localhost:3000/api/v1/users/${id}`)
+  return fetch(`http://localhost:3001/api/v1/users/${id}`)
   .then(response => response.json())
   .then(data => data)
 }
