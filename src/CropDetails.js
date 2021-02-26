5513b94c579d7a6e120f80765de670671ba88a5f
Brian Forbes <brianandrewf81@gmail.com>
2021-02-25 17:36:38 -0700

    Add all elements to display all crop details in CropDetails component.

--- src/CropDetails.js
+++ src/CropDetails.js
@@ -3,12 +3,19 @@ import React from 'react';
 
 
 const CropDetails = ({data, id}) => {
-  console.log(id)
+
   const thisCrop = data.find(crop => crop.id === parseInt(id))
-  console.log('Single crop', thisCrop)
   return (
-    <div>
-      <img src={thisCrop.photoLinks[0]} />
+    <div className="crop-details-container">
+      <img className="crop-details-image" src={thisCrop.photoLinks[0]} />
+      <h1 className="crop-details-name">Name: {thisCrop.name}</h1>
+      <p className="crop-details-description" >Description: {thisCrop.description}</p>
+      <p className="crop-sun-requirements">Sun Requirement: {thisCrop.sunReqs}</p>
+      <p className="sowing-method">Sowing Method: {thisCrop.sowingMethod}</p>
+      <p className="crop-spread">Spread: {thisCrop.spread}cm</p>
+      <p className="row-spacing">Row Spacing: {thisCrop.rowSpacing}cm</p>
+      <p className="crop-height">Height: {thisCrop.height}cm</p>
+      <p className="maturity">Days To Maturity: {thisCrop.daysToMaturity}</p>
     </div>
   )
 }
