7bd9bb62e027ab9048bbcb1723da15d441d3e7cd
mattdean <matthewallandean@gmail.com>
2021-03-01 20:07:29 -0700

    Fix images

--- src/Crop.js
+++ src/Crop.js
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom'
 
 const Crop = ({id, name, image, toggleFavorite}) => {
     return (
-      
         <div className='seed-packet'>
           <section className="seed-packet-top">
             <h3>{name}</h3>
             <article onClick={() => toggleFavorite(id)} className="toggle">Add to Garden</article>
           </section>
           <Link to={`/${id}`} id={id} aria-label={name}>
-          <img src={image} alt={`${name}`}></img>
+            <img className = "crop-image" src={image} alt={`${name}`} />
           </Link>
           <p className="footnote">© Urban Native</p>
         </div>
