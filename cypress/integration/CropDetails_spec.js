e322fba9ff10b5c04a358e92d2e7659d868dc79a
Brian Forbes <brianandrewf81@gmail.com>
2021-03-01 21:23:47 -0700

    CropDetails component should contain an image of the crop

--- cypress/integration/CropDetails_spec.js
+++ cypress/integration/CropDetails_spec.js
@@ -100,12 +100,29 @@ describe('CropDetails', () => {
       .get('p[class=crop-height]').contains(`15cm`)
     })
 
+    it('CropDetails component should have a height property measured in centimeters', () => {
+      cy
+      .get('a[id=18]').click()
+      cy
+      .get('p[class=maturity]').contains(`Raspberry bush growth depends on cultivar and environment`)
+      cy
+      .get('button[class=back-button]').click()
+      cy
+      .get('a[id=16]').click()
+      cy
+      .get('p[class=maturity]').contains(`200`)
+    })
 
-
-    // it.only('CropDetails component should have a sun requirement', () => {
-    //
-    // })
-    // it.only('CropDetails component should have a sun requirement', () => {
-    //
-    // })
+    it.only('CropDetails component should have an image of the crop', () => {
+      cy
+      .get('a[id=16]').click()
+      cy
+      .get('img').should('have.attr', 'src', 'https://images.unsplash.com/photo-1560806887-1e4cd0b6cbd6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHw%3D&w=1000&q=80')
+      cy
+      .get('button[class=back-button]').click()
+      cy
+      .get('a[id=7]').click()
+      cy
+      .get('img').should('have.attr', 'src', 'https://s3.amazonaws.com/openfarm-project/production/media/pictures/attachments/551e27c53732390003830100.jpg?1428039620')
+    })
 });
