3ab32516f5403f23c93a4325a68fa7dd2601bbf1
Brian Forbes <brianandrewf81@gmail.com>
2021-03-01 20:26:56 -0700

    Test for CropDetails component to have a crop name, description and sun requirements properties.

--- cypress/integration/CropDetails_spec.js
+++ cypress/integration/CropDetails_spec.js
@@ -6,7 +6,7 @@ describe('CropDetails', () => {
         .visit(baseUrl);
     });
 
-    it.only('should be able to navigate to the crop details view by clicking on a crop', () => {
+    it('should be able to navigate to the crop details view by clicking on a crop', () => {
       cy
       .get('a[id=5]').click()
       cy
@@ -15,13 +15,49 @@ describe('CropDetails', () => {
       .get('a[id=25]').click()
     })
 
-    it.only('CropDetails should contain a heading with the crop name', () => {
+    it('CropDetails should contain a heading with the crop name', () => {
       cy
       .get('a[id=5]').click()
       cy
-      .get('h1[class=crop-details-name]')
-      .contains('Carrots')
+      .get('h1[class=crop-details-name]').contains('Carrots')
     })
 
-    
+    it.only('CropDetails component should have a description', () => {
+      cy
+      .get('a[id=25]').click()
+      cy
+      .get('p[class=crop-details-description]').contains('Radishes are fast-growing')
+      cy
+      .get('button[class=back-button]').click()
+      cy
+      .get('a[id=17]').click()
+      cy
+      .get('p[class=crop-details-description]').contains('Strawberries are a hybrid species')
+    })
+
+    it.only('CropDetails component should have a sun requirement', () => {
+      cy
+      .get('a[id=15]').click()
+      cy
+      .get('p[class=crop-sun-requirements]').contains('Full Sun')
+      cy
+      .get('button[class=back-button]').click()
+      cy
+      .get('a[id=19]').click()
+      cy
+      .get('p[class=crop-sun-requirements]').contains('Full Sun')
+    })
+
+    // it.only('CropDetails component should have a sun requirement', () => {
+    //
+    // })
+    // it.only('CropDetails component should have a sun requirement', () => {
+    //
+    // })
+    // it.only('CropDetails component should have a sun requirement', () => {
+    //
+    // })
+    // it.only('CropDetails component should have a sun requirement', () => {
+    //
+    // })
 });
