f3ad84ca47139081dce9d4cb342fa7b6848b601d
Brian Forbes <brianandrewf81@gmail.com>
2021-03-02 19:31:43 -0700

    Create test file for grow info component and add necessary class names

---
+++ cypress/integration/GrowInfo_spec.js
@@ -0,0 +1,13 @@
+describe('CropDetails', () => {
+    const baseUrl = 'http://localhost:3000';
+
+    beforeEach(() => {
+        cy
+        .visit(baseUrl);
+    });
+
+    it.only('should navigate to a grow info component when the top right button is clicked', () => {
+      cy
+      .get('.header-button').click()
+    })
+});
