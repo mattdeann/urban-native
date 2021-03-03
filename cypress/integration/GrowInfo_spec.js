8ef2125d756a0729908ff13d0d6dbafaa049efbc
Brian Forbes <brianandrewf81@gmail.com>
2021-03-02 19:36:33 -0700

    Test paragraph section contains general grow info

--- cypress/integration/GrowInfo_spec.js
+++ cypress/integration/GrowInfo_spec.js
@@ -3,13 +3,18 @@ describe('CropDetails', () => {
 
     beforeEach(() => {
         cy
-        .visit(baseUrl);
+        .visit(baseUrl)
+        cy
+        .get('.header-button').click()
     });
 
     it.only('should navigate to a grow info component when the top right button is clicked', () => {
       cy
-      .get('.header-button').click()
-      cy
       .get('.grow-info-heading').contains('Gardening on the Front Range')
+    })
+
+    it.only('should contain a paragraph section with general grow info', () => {
+      cy
+      .get('.colorado-grow-info').contains('Gardening in Colorado can be challenging')
     })
 });
